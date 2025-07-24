9217bf7f526d668d2f45442ec691457117d38d1e
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-07-24 16:59:07 +0530

    navbar login logout state managed and navigation managed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,13 +2,15 @@ import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
+//waiter
 
 export const AppContextProvider = ({ children }) => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
 
-    const value = {navigate, user, setUser, isSeller, setIsSeller};
+    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin};
     return <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
@@ -17,3 +19,4 @@ export const AppContextProvider = ({ children }) => {
 export const useAppContext = () => {
     return useContext(AppContext);
 }
+//calling the waiter
