f3cb526f367b2ce026ab78232ca8b71fa0cd0054
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-08-07 18:20:31 +0530

    frontend complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,17 @@ import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
+import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Order from './pages/seller/Orders'
+
 
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext();
+  const {showUserLogin, isSeller} = useAppContext();
 
   return (
     <div>
@@ -33,7 +39,11 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
-
+          <Route path="/seller" element={isSeller? <SellerLayout/> : <SellerLogin />}>
+            <Route  index element={isSeller ? <AddProduct/> : null}/>
+            <Route  path='product-list' element={<ProductList/>}/>
+            <Route  path='orders' element={<Order/>}/>
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
