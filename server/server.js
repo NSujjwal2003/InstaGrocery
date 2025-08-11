ea1ce9ef7ecfe7384cdfcddaabd6b32d8290dad7
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-08-11 14:10:44 +0530

    seller and user services done

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/seller', sellerRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
